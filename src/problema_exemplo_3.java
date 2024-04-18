89d3869e5cb9d01803d61ce873ec5142944a1b8f
Thiaguiner <tcarneiro990@gmail.com>
2024-04-18 12:24:48 -0300

    criando metodos para beneficiar e reaproveitar o codigo

--- src/problema_exemplo_3.java
+++ src/problema_exemplo_3.java
@@ -20,11 +20,8 @@ public class problema_exemplo_3 {
         y.b = sc.nextDouble();
         y.c = sc.nextDouble();
 
-        double p = (x.a + x.b + x.c) / 2;
-        double areaX = Math.sqrt(p * (p -x.a) * (p - x.b) * (p - x.c));//area do x
-
-        p = (y.a + y.b + y.c) / 2;
-        double areaY = Math.sqrt(p * (p -y.a) * (p - y.b) * (p - y.c));//area do y
+        double areaX = x.area();
+        double areaY = y.area();
 
         System.out.printf("Triangulo X area: %.4f%n", areaX);
         System.out.printf("Triangulo Y area: %.4f%n", areaY);
