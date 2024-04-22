2596aa5daa15cbc37f4893bde68c65da9acd2d8a
Thiaguiner <tcarneiro990@gmail.com>
2024-04-22 13:25:08 -0300

    exercicio de membros estaticos

---
+++ src/exercicio_static/Program.java
@@ -0,0 +1,19 @@
+package exercicio_static;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("What is the dollar price? ");
+        double dollarPrice = sc.nextDouble();
+        System.out.println("How many dollars will be bought? ");
+        double amount = sc.nextDouble();
+        double result = CurrencyConverter.dollarToReal(amount, dollarPrice);
+        System.out.println("Amount to be pais in reais = " + result);
+
+        sc.close();
+    }
+}
