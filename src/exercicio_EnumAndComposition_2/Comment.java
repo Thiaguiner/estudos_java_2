af9db68afb2c96b2684559a58a7945857f9d3569
Thiaguiner <tcarneiro990@gmail.com>
2024-05-06 13:30:50 -0300

    exercicio de enum e composição

---
+++ src/exercicio_EnumAndComposition_2/Comment.java
@@ -0,0 +1,20 @@
+package exercicio_EnumAndComposition_2;
+
+public class Comment {
+
+    private String text;
+
+    public Comment() {
+    }
+    public Comment(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
