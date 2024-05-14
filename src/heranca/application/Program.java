dae0fbf8971c4c7f8e1be425fba2ce1909d0ebf0
Thiaguiner <tcarneiro990@gmail.com>
2024-05-14 00:05:57 -0300

    polimorfismo

--- src/heranca/application/Program.java
+++ src/heranca/application/Program.java
@@ -4,19 +4,18 @@ import heranca.application.entities.Account;
 import heranca.application.entities.BusinessAccount;
 import heranca.application.entities.SavingsAccount;
 
+import java.util.AbstractCollection;
+
 public class Program {
     public static void main(String[] args) {
 
-        Account acc1 = new Account(1001, "Alex", 1000.0);
-        acc1.withdraw(200);
-        System.out.println(acc1.getBalance());
+        Account x = new Account(1020, "Alex", 1000.0);
+        Account y = new SavingsAccount(1024, "Joao", 1000.0, 0.01);
 
-        Account acc2 = new SavingsAccount(1002, "Thiago", 1000.0, 0.01);
-        acc2.withdraw(200);
-        System.out.println(acc2.getBalance());
+        x.withdraw(50);
+        y.withdraw(50);
 
-        Account acc3 = new BusinessAccount(1003, "Thiago", 1000.0, 500.0);
-        acc3.withdraw(200);
-        System.out.println(acc3.getBalance());
+        System.out.println(x.getBalance());
+        System.out.println(y.getBalance());
     }
 }
