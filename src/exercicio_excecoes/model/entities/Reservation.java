e1b5cc479e6155e8632522e5694b6dbc199a88fc
Thiaguiner <tcarneiro990@gmail.com>
2024-05-16 18:43:11 -0300

    solução de exercicio pt1

---
+++ src/exercicio_excecoes/model/entities/Reservation.java
@@ -0,0 +1,74 @@
+package exercicio_excecoes.model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+    private Integer roomNumber;
+    private Date checkin;
+    private Date checkout;
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    public Reservation() {
+    }
+
+    public Reservation(Integer roomNumber, Date checkin, Date checkout) {
+        this.roomNumber = roomNumber;
+        this.checkin = checkin;
+        this.checkout = checkout;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public Date getCheckin() {
+        return checkin;
+    }
+
+    public Date getCheckout() {
+        return checkout;
+    }
+    public long duration(){
+        long diff = checkout.getTime() - checkin.getTime();
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);//converte diff que estava em milisegundos para dia
+    }
+    public void updateDays(Date checkin, Date checkout){
+        this.checkin = checkin;
+        this.checkout = checkout;
+    }
+    @Override
+    public String toString(){
+        return "Room "
+                + roomNumber
+                + ", check-in: "
+                + sdf.format(checkin)
+                + ", check-out: "
+                + sdf.format(checkout)
+                + ", "
+                + duration()
+                + " nights";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
