113b223ae62f809c0fa61e160e7619c02beca303
Thiaguiner <tcarneiro990@gmail.com>
2024-05-21 15:20:25 -0300

    primeira forma de fazer

---
+++ src/exercicio_fixacao_tratamento_excecoes/application/Program.java
@@ -0,0 +1,37 @@
+package exercicio_fixacao_tratamento_excecoes.application;
+import exercicio_fixacao_tratamento_excecoes.entities.Account;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Informe os dados da conta");
+        System.out.print("Numero: ");
+        int number = sc.nextInt();
+        System.out.print("Titular: ");
+        sc.nextLine();
+        String holder = sc.nextLine();
+        System.out.print("Saldo inicial: ");
+        double balance = sc.nextDouble();
+        System.out.print("Limite de saque: ");
+        double withdrawLimit = sc.nextDouble();
+
+        Account acc = new Account(number, holder, balance, withdrawLimit);
+
+        System.out.println();
+        System.out.print("Informe uma quantia para saque: ");
+        double amount = sc.nextDouble();
+        
+        if (amount > withdrawLimit){
+            System.out.println("Erro de saque: A quantia excede o limite de saque!");
+        } else if (amount > acc.getBalance()) {
+            System.out.println("Erro de saque: Saldo insuficiente");
+        }
+        else {
+            acc.withdraw(amount);
+            System.out.println("Novo saldo: " + acc.getBalance());
+        }
+
+        sc.close();
+    }
+}
